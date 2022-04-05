82874dc6f9d3462b4e305b86c4675c3f5b7bce59
Michael J <johnson_michael1329@yahoo.com>
2022-04-05 16:02:18 -0500

    Created GET functionality where all items display in the correct categories + added id field to Super serializers.py file.

--- supers/views.py
+++ supers/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework import status
 from rest_framework.response import Response
+
+from super_types.models import SuperTypes
 from .serializers import SuperSerializer
 from .models import Super
 # Create your views here.
@@ -9,16 +11,23 @@ from .models import Super
 @api_view(['GET', 'POST'])
 def super_list(request):
   super_type_param = request.query_params.get('type')
-  print(super_type_param)
   supers = Super.objects.all()
-
   if super_type_param:
     supers = supers.filter(super_type__type=super_type_param)
     serializer = SuperSerializer(supers, many=True)
     return Response(serializer.data)
-  if request.method == 'GET':
-    serializer = SuperSerializer(supers, many=True)
-    return Response(serializer.data)
+  elif request.method == 'GET':
+    super_types = SuperTypes.objects.all()
+    custom_response_dictionary = {}
+    for supertype in super_types:
+      supers = Super.objects.filter(super_type_id=supertype.id)
+      super_serializer = SuperSerializer(supers, many=True)
+      if supertype.id == 1:
+        custom_response_dictionary['Heroes'] = super_serializer.data
+      if supertype.id == 2:
+           custom_response_dictionary['Villains'] = super_serializer.data
+      
+    return Response(custom_response_dictionary)
   elif request.method == 'POST':
     serializer = SuperSerializer(data=request.data)
     serializer.is_valid(raise_exception=True)
